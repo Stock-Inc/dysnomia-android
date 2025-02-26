fe5f59460b2bc8419f61f47969dbc032283a5dd3
Alnoer <getmanovv950@gmail.com>
2025-02-26 22:25:27 +0300

    revert hilt

--- app/src/main/java/su/femboymatrix/buttplug/FemboyApplication.kt
+++ app/src/main/java/su/femboymatrix/buttplug/FemboyApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import su.femboymatrix.buttplug.data.DefaultFemboyAppContainer
 import su.femboymatrix.buttplug.data.FemboyAppContainer
 
-
 class FemboyApplication : Application() {
     lateinit var container: FemboyAppContainer
 
--- app/src/main/java/su/femboymatrix/buttplug/MainActivity.kt
+++ app/src/main/java/su/femboymatrix/buttplug/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import su.femboymatrix.buttplug.ui.theme.FemboyMatrixTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
