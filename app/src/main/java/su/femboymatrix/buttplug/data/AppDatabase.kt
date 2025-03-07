4b5e9ec2249e635f95d1a7dc69afd4142c836fd2
Alnoer <getmanovv950@gmail.com>
2025-03-07 19:59:58 +0300

    exportSchema = false

--- app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
+++ app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package su.femboymatrix.buttplug.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ConsoleHistoryEntity::class], version = 1)
+@Database(entities = [ConsoleHistoryEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun consoleDao(): ConsoleDao
 }
