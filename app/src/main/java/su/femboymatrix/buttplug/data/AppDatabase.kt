3a15f45e87a3dbd5440bee1498902a27aca2f27f
Alnoer <getmanovv950@gmail.com>
2025-03-07 20:03:03 +0300

    Revert "exportSchema = false"
    This reverts commit 4b5e9ec2249e635f95d1a7dc69afd4142c836fd2.

--- app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
+++ app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package su.femboymatrix.buttplug.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ConsoleHistoryEntity::class], version = 1, exportSchema = false)
+@Database(entities = [ConsoleHistoryEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun consoleDao(): ConsoleDao
 }
