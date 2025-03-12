b283a145843f173c424d0f363fd7f1d8efef1a47
Alnoer <getmanovv950@gmail.com>
2025-03-13 00:40:23 +0300

    retrofit error catching

--- app/src/main/java/su/femboymatrix/buttplug/ui/screen/chat/ChatViewModel.kt
+++ app/src/main/java/su/femboymatrix/buttplug/ui/screen/chat/ChatViewModel.kt
@@ -10,6 +10,8 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 import su.femboymatrix.buttplug.data.ChatHistoryEntity
 import su.femboymatrix.buttplug.data.FemboyNetworkRepository
 import su.femboymatrix.buttplug.data.FemboyOfflineRepository
@@ -38,19 +40,35 @@ class ChatViewModel @Inject constructor(
     fun sendMessage() {
         if (_chatUiState.value.text.isNotEmpty()) {
             viewModelScope.launch {
-                femboyOfflineRepository.addToHistory(
-                    ChatHistoryEntity(
-                        command = _chatUiState.value.text.trim(),
-                        result = femboyNetworkRepository.sendMessage(
-                            _chatUiState.value.text.trim()
+                try {
+                    femboyOfflineRepository.addToHistory(
+                        ChatHistoryEntity(
+                            command = _chatUiState.value.text.trim(),
+                            result = femboyNetworkRepository.sendMessage(
+                                _chatUiState.value.text.trim()
+                            )
                         )
                     )
-                )
-            }
-            _chatUiState.update {
-                it.copy(
-                    text = ""
-                )
+                } catch (e: IOException) {
+                    femboyOfflineRepository.addToHistory(
+                        ChatHistoryEntity(
+                            command = _chatUiState.value.text.trim(),
+                            result = "Error connecting to the server:\n$e"
+                        )
+                    )
+                } catch (e: HttpException) {
+                    femboyOfflineRepository.addToHistory(
+                        ChatHistoryEntity(
+                            command = _chatUiState.value.text.trim(),
+                            result = "Error connecting to the server:\n$e"
+                        )
+                    )
+                }
+                _chatUiState.update {
+                    it.copy(
+                        text = ""
+                    )
+                }
             }
         }
     }
