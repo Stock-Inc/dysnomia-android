780b78778806b5fab5e4024da622b7352f7b5030
Alnoer <getmanovv950@gmail.com>
2025-03-16 11:50:27 +0300

    force dark theme

--- app/src/main/java/su/femboymatrix/buttplug/ui/theme/Theme.kt
+++ app/src/main/java/su/femboymatrix/buttplug/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package su.femboymatrix.buttplug.ui.theme
 
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -88,7 +87,7 @@ private val darkScheme = darkColorScheme(
 
 @Composable
 fun FemboyMatrixTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true, // isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
