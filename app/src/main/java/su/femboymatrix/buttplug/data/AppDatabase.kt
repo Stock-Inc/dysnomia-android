286b4e2d7de7a794127742273acae0992e54a46e
Alnoer <getmanovv950@gmail.com>
2025-03-16 13:53:40 +0300

    database version downgrade because of broken schema

--- app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
+++ app/src/main/java/su/femboymatrix/buttplug/data/AppDatabase.kt
@@ -1,13 +1,11 @@
 package su.femboymatrix.buttplug.data
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
     entities = [ChatHistoryEntity::class],
-    version = 5,
-    autoMigrations = [AutoMigration(from = 4, to = 5)]
+    version = 4
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun chatDao(): ChatDao
