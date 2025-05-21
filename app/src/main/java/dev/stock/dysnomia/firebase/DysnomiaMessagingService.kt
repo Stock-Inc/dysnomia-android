8fdd468edbaeeb0d4fea7452c202b4fb03d91da2
alnoer <97702492+Alnoerr@users.noreply.github.com>
2025-05-21 19:41:18 +0300

    message fix

--- app/src/main/java/dev/stock/dysnomia/firebase/DysnomiaMessagingService.kt
+++ app/src/main/java/dev/stock/dysnomia/firebase/DysnomiaMessagingService.kt
@@ -20,50 +20,12 @@ class DysnomiaMessagingService : FirebaseMessagingService() {
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-    // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${remoteMessage.from}")
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
-
-            // Check if data needs to be processed by long-running job
-//            if (isLongRunningJob()) {
-            // For long-running tasks (10 seconds or more) use WorkManager.
-//                scheduleJob()
-//            } else {
-            // Handle message within 10 seconds
-//                handleNow()
-//            }
-        }
-
-        // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
-            Log.d(TAG, "Message Notification Body: ${it.body}")
-            sendNotification(it.title ?: "Dysnomia", it.body ?: "")
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        val title = remoteMessage.data.getOrElse("title") { "Dysnomia" }
+        val body = remoteMessage.data.getOrElse("body") { "" }
+        sendNotification(title, body)
     }
-    // [END receive_message]
 
-//    private fun isLongRunningJob() = true
-
-    // [START on_new_token]
     /**
      * Called if the FCM registration token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the
@@ -71,13 +33,8 @@ class DysnomiaMessagingService : FirebaseMessagingService() {
      */
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
-
-        // If you want to send messages to this application instance or
-        // manage these apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-        sendRegistrationToServer(token)
+//        sendRegistrationToServer(token)
     }
-    // [END on_new_token]
 
     /**
      * Schedule async work using WorkManager.
@@ -104,10 +61,10 @@ class DysnomiaMessagingService : FirebaseMessagingService() {
      *
      * @param token The new token.
      */
-    private fun sendRegistrationToServer(token: String?) {
-        // TODO: Implement this method to send token to your app server.
-        Log.d(TAG, "sendRegistrationTokenToServer($token)")
-    }
+//    private fun sendRegistrationToServer(token: String?) {
+//        // TODO: Implement this method to send token to your app server.
+//        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+//    }
 
     /**
      * Create and show a simple notification containing the received FCM message.
@@ -144,7 +101,7 @@ class DysnomiaMessagingService : FirebaseMessagingService() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 channelId,
-                "New message",
+                "Global chat",
                 NotificationManager.IMPORTANCE_DEFAULT,
             )
             notificationManager.createNotificationChannel(channel)
