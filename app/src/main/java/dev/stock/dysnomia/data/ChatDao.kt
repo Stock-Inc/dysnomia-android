1d21ec2e0bd29b3d821569b8ef253b9c5f6b63b3
Alnoer <97702492+Alnoerr@users.noreply.github.com>
2025-06-08 18:44:58 +0300

    Upsert in ChatDao test

--- app/src/main/java/dev/stock/dysnomia/data/ChatDao.kt
+++ app/src/main/java/dev/stock/dysnomia/data/ChatDao.kt
@@ -1,15 +1,14 @@
 package dev.stock.dysnomia.data
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import dev.stock.dysnomia.model.MessageEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ChatDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Upsert
     suspend fun addToHistory(messageEntity: MessageEntity)
 
     @Query("SELECT * from chat_history")
