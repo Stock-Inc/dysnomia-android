23bc54b888e7aec1efbfe137a3931d09a8aa2d4e
Alnoer <97702492+Alnoerr@users.noreply.github.com>
2025-06-12 16:13:58 +0300

    RECONNECTION_TIME removed

--- app/src/main/java/dev/stock/dysnomia/utils/Constants.kt
+++ app/src/main/java/dev/stock/dysnomia/utils/Constants.kt
@@ -3,7 +3,6 @@ package dev.stock.dysnomia.utils
 const val API_BASE_URL = "https://api.femboymatrix.su/"
 const val WEBSOCKETS_BASE_URL = "https://api.femboymatrix.su/ws/websocket"
 const val SHARING_TIMEOUT_MILLIS = 5_000L
-const val RECONNECTION_TIME = 3_000L
 const val HISTORY_TOPIC = "/topic/history"
 const val HISTORY_APP = "/app/history"
 const val MESSAGE_TOPIC = "/topic/message"
